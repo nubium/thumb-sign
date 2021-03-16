06bb2db63f616156009d2b1367ef9e3a3ae13de4
Jiri Hynek <jiri.hynek@nubium.cz>
2021-03-16 11:47:26 +0100

    thumb signing service from ulozto git

---
+++ src/ThumbnailSecurityChecksumService.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+namespace Nubium\ThumbSign;
+
+use DateInterval;
+use DateTimeImmutable;
+use DateTimeZone;
+use Nette\Http\Url;
+
+class ThumbnailSecurityChecksumService
+{
+	/** @var string */
+	private $secret;
+
+
+	public function __construct(string $secret)
+	{
+		$this->secret = $secret;
+	}
+
+
+	public function signUrl(Url $url, ?DateInterval $customValidityInterval = null): Url
+	{
+		$validUntil = $this->computeValidity($customValidityInterval);
+
+		$sg = md5(
+			$url->getPath()
+			. $this->secret
+			. (string) $validUntil->format('U')
+		, true);
+
+		return $url
+			->setQueryParameter('vt', (string) $validUntil->format('U'))
+			->setQueryParameter('sg', trim(strtr(base64_encode($sg), '+/', '-_'), '='))
+			->setQueryParameter('bl', ''); # blank parameter because of prevent wrong appended parameters
+	}
+
+
+	protected function getCurrentTime(): DateTimeImmutable
+	{
+		return new DateTimeImmutable('now', new DateTimeZone('Europe/Prague'));
+	}
+
+
+	private function computeValidity(?DateInterval $customValidityInterval = null): DateTimeImmutable
+	{
+		$currentTime = $this->getCurrentTime();
+
+		if (!$customValidityInterval) {
+			$validity = $currentTime
+				->modify('tomorrow 23:59:59');
+
+			// if less than 5 minutes till midnight, add one more day to validity
+			if ($currentTime->add(new DateInterval('PT5M')) > $currentTime->modify('tomorrow')) {
+				$validity = $validity->modify('tomorrow 23:59:59');
+			}
+		} else {
+			$validity = $currentTime->add($customValidityInterval);
+		}
+
+		return $validity;
+	}
+
+}
---
+++ test/ThumbnailSecurityChecksumServiceTest.php
@@ -0,0 +1,62 @@
+<?php declare(strict_types=1);
+namespace Nubium\ThumbSign\Test;
+
+use DateTimeImmutable;
+use Nette\Http\Url;
+use Nubium\ThumbSign\ThumbnailSecurityChecksumService;
+use PHPUnit\Framework\TestCase;
+
+class ThumbnailSecurityChecksumServiceTest extends TestCase
+{
+
+
+	/**
+	 * @dataProvider dpParams
+	 */
+	public function testParamsAreCorrect(Url $url, DateTimeImmutable $currentTime, string $expectedUrl): void
+	{
+		$service = $this->createThumbnailSecurityChecksumService($currentTime);
+		$signedUrl = $service->signUrl($url);
+
+		$this->assertEquals($expectedUrl, $signedUrl->getAbsoluteUrl());
+	}
+
+
+	public function dpParams(): array
+	{
+		return [
+			'generic' => [
+				'url' => new Url('https://thumb.uloz.to/d/7/6/d767ad66567c4968c92e006b6282e14f.260x170.jpg'),
+				'currentTime' => new DateTimeImmutable('2019-09-26 15:32', new \DateTimeZone('Europe/Prague')),
+				'expectedUrl' => 'https://thumb.uloz.to/d/7/6/d767ad66567c4968c92e006b6282e14f.260x170.jpg?vt=' . (new DateTimeImmutable('2019-09-27 23:59:59', new \DateTimeZone('Europe/Prague')))->format('U') .'&sg=Z_AdRmge8ecZJalf7W9SQQ&bl=',
+			],
+			'5 minutest till midnight' => [
+				'url' => new Url('https://thumb.uloz.to/d/7/6/d767ad66567c4968c92e006b6282e14f.260x170.jpg'),
+				'currentTime' => new DateTimeImmutable('2019-09-27 23:57:12', new \DateTimeZone('Europe/Prague')),
+				'expectedUrl' => 'https://thumb.uloz.to/d/7/6/d767ad66567c4968c92e006b6282e14f.260x170.jpg?vt=' . (new DateTimeImmutable('2019-09-29 23:59:59', new \DateTimeZone('Europe/Prague')))->format('U') .'&sg=UrKNcXxIG1bpaxt3efcINQ&bl=',
+			],
+		];
+	}
+
+
+	public function testCustomValidityInterval(): void
+	{
+		$service = $this->createThumbnailSecurityChecksumService(new DateTimeImmutable('2020-01-23 00:11:22', new \DateTimeZone('Europe/Prague')));
+		$signedUrl = $service->signUrl(new Url('http://example.com/path'), new \DateInterval('P2DT3H'));
+
+		$this->assertSame(
+			'http://example.com/path?vt=' . (new DateTimeImmutable('2020-01-25 03:11:22', new \DateTimeZone('Europe/Prague')))->format('U') . '&sg=nAWC_KNIXHJ5EAHMplehrw&bl=',
+			$signedUrl->getAbsoluteUrl()
+		);
+	}
+
+
+	private function createThumbnailSecurityChecksumService(DateTimeImmutable $currentTime): ThumbnailSecurityChecksumService
+	{
+		$mock = \Mockery::mock(ThumbnailSecurityChecksumService::class . '[getCurrentTime]', ['imgTopSecret']);
+		$mock->shouldAllowMockingProtectedMethods();
+		$mock->shouldReceive('getCurrentTime')->andReturn($currentTime);
+
+		return $mock;
+	}
+}
